6388695511d5293116b901583d24a100cb73eebd
duong-s-97 <duongdev97@gmail.com>
2023-09-09 00:49:33 +0700

    map data dasboard

--- src/views/dashboard/TotalEarning.js
+++ src/views/dashboard/TotalEarning.js
@@ -1,4 +1,7 @@
+import React, { useEffect } from 'react'
+
 // ** MUI Imports
+
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import Avatar from '@mui/material/Avatar'
@@ -14,35 +17,63 @@ import MenuDown from 'mdi-material-ui/MenuDown'
 
 import DotsVertical from 'mdi-material-ui/DotsVertical'
 
-const data = [
-  {
-    progress: 75,
-    imgHeight: 20,
-    title: 'Month Income',
-    color: 'primary',
-    subtitle: 'growthIncome',
-    imgSrc: '/images/cards/logo-zipcar.png'
-  },
-  {
-    progress: 50,
-    color: 'info',
-    imgHeight: 27,
-    title: 'Month Transaction',
-    subtitle: 'growthTransaction',
-    imgSrc: '/images/cards/logo-bitbank.png'
-  },
-  {
-    progress: 20,
-    imgHeight: 20,
-    title: 'Month Customer',
-    color: 'secondary',
-    subtitle: 'growthCustomer',
-    imgSrc: '/images/cards/logo-aviato.png'
-  }
-]
+import { useDispatch, useSelector } from 'react-redux'
+import { dashboardActions, makeSelectDashBoard } from './dashboardSlice'
+
+// const data = [
+//   {
+//     progress: 75,
+//     imgHeight: 20,
+//     title: 'Month Income',
+//     color: 'primary',
+//     subtitle: 'growthIncome',
+//     imgSrc: '/images/cards/logo-zipcar.png'
+//   },
+//   {
+//     progress: 50,
+//     color: 'info',
+//     imgHeight: 27,
+//     title: 'Month Transaction',
+//     subtitle: 'growthTransaction',
+//     imgSrc: '/images/cards/logo-bitbank.png'
+//   },
+//   {
+//     progress: 20,
+//     imgHeight: 20,
+//     title: 'Month Customer',
+//     color: 'secondary',
+//     subtitle: 'growthCustomer',
+//     imgSrc: '/images/cards/logo-aviato.png'
+//   }
+// ]
 
 const TotalEarning = props => {
   const { dataDashboard } = props
+
+  const dispatch = useDispatch()
+  const globalDataDaboard = useSelector(makeSelectDashBoard)
+
+  // Call api khi lần đầu vào trang
+  useEffect(() => {
+    dispatch(dashboardActions.getListDashBoard())
+  }, [])
+
+  // tổng tiền dontaion
+  const totalAmount = dataDashboard.reduce((total, donation) => {
+    return total + donation.amount
+  }, 0)
+
+  // 3 donation có amount lớn nhất
+
+  let donationsCopy = dataDashboard.slice()
+
+  const getTopThreeDonations = donations => {
+    donations.sort((a, b) => b.amount - a.amount)
+
+    return donations.slice(0, 3)
+  }
+
+  const top3Donations = getTopThreeDonations(donationsCopy)
 
   const growth = {
     growthIncome: (dataDashboard?.monthIncome / dataDashboard?.pastIncome - 1) * 100,
@@ -64,23 +95,23 @@ const TotalEarning = props => {
       <CardContent sx={{ pt: theme => `${theme.spacing(2.25)} !important` }}>
         <Box sx={{ mb: 1.5, display: 'flex', alignItems: 'center' }}>
           <Typography variant='h4' sx={{ fontWeight: 600, fontSize: '2.125rem !important' }}>
-            $24,895
+            ${totalAmount}
           </Typography>
         </Box>
 
         <Typography component='p' variant='caption' sx={{ mb: 9 }} />
 
-        {data.map((item, index) => {
+        {top3Donations.map((item, index) => {
           return (
             <Box
               key={item.title}
               sx={{
                 display: 'flex',
                 alignItems: 'center',
-                ...(index !== data.length - 1 ? { mb: 8.5 } : {})
+                ...(index !== top3Donations.length - 1 ? { mb: 8.5 } : {})
               }}
             >
-              <Avatar
+              {/* <Avatar
                 variant='rounded'
                 sx={{
                   mr: 3,
@@ -90,7 +121,7 @@ const TotalEarning = props => {
                 }}
               >
                 <img src={item.imgSrc} alt={item.title} height={item.imgHeight} />
-              </Avatar>
+              </Avatar> */}
               <Box
                 sx={{
                   width: '100%',
@@ -103,9 +134,9 @@ const TotalEarning = props => {
                 <Box sx={{ marginRight: 2, display: 'flex', flexDirection: 'column' }}>
                   <Box className='d-flex justify-content-center align-items-center'>
                     <Typography variant='body2' sx={{ mb: 0.5, fontWeight: 600, color: 'text.primary' }}>
-                      {item.title}
+                      {item.partnerName}
                     </Typography>
-                    <Box
+                    {/* <Box
                       className='d-flex justify-content-center align-items-center'
                       sx={{ color: growth[item.subtitle] * 100 > 0 ? 'success.main' : 'red' }}
                     >
@@ -122,14 +153,14 @@ const TotalEarning = props => {
                       >
                         {growth[item.subtitle] * 100}
                       </Typography>
-                    </Box>
+                    </Box> */}
                   </Box>
-                  <Typography variant='caption'>{item.subtitle}</Typography>
+                  <Typography variant='caption'>{item.programName}</Typography>
                 </Box>
 
                 <Box sx={{ minWidth: 85, display: 'flex', flexDirection: 'column' }}>
                   <Typography variant='body2' sx={{ mb: 2, fontWeight: 600, color: 'text.primary' }}>
-                    {item.amount}
+                    ${item.amount}
                   </Typography>
                   {/* <LinearProgress color={item.color} value={item.progress} variant='determinate' /> */}
                 </Box>
